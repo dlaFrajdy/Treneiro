cb277caf91f3ab93288d0704a544401bc0a65c0f
hexfffDS <hexfff@gmail.com>
2021-05-02 17:42:57 +0200

    SoftDelete+QueryFilter

--- Treneiro/Data/ApplicationDbContext.cs
+++ Treneiro/Data/ApplicationDbContext.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Treneiro.Data
 {
@@ -12,6 +14,40 @@ namespace Treneiro.Data
             : base(options)
         {
         }
-        //dodać softdelete aouto i queryfilter
+        public override int SaveChanges()
+        {
+            UpdateSoftDeleteStatuses();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            UpdateSoftDeleteStatuses();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        private void UpdateSoftDeleteStatuses()
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues["Deleted"] = false;
+                        break;
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues["Deleted"] = true;
+                        break;
+                }
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Model_Name>().HasQueryFilter(model => !model.Deleted); //example
+        }
     }
 }
